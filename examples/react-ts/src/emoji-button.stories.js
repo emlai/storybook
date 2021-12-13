f12ed19b7008e404089a924c4999bd2d96c79637
Emil Laine <emil.laine@reaktor.com>
2021-12-13 11:22:52 +0200

    repro

--- examples/react-ts/src/emoji-button.stories.js
+++ examples/react-ts/src/emoji-button.stories.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { EmojiButton } from './emoji-button';
+import {Paper} from "@material-ui/core";
 
 export default { component: EmojiButton, title: 'Examples / Emoji Button' };
 
-export const WithArgs = (args) => <EmojiButton {...args} />;
+export const WithArgs = (args) => <EmojiButton {...args} component={Paper} />;
 WithArgs.args = { label: 'With args' };
 export const Basic = () => <EmojiButton label="Click me" />;
